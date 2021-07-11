c75732cf4494b6224abadc96f2b292f7270f3cd9
XxcuriousxX <test@gmail.com>
2021-07-11 16:14:01 +0300

    see now

---
+++ src/app/main-view/main-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-view',
+  templateUrl: './main-view.component.html',
+  styleUrls: ['./main-view.component.css']
+})
+export class MainViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
