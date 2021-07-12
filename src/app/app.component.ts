68533c2c9496be99d2b99d82c0bb4e58bf431008
XxcuriousxX <test@gmail.com>
2021-07-12 17:33:58 +0300

    login html

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
   title = 'myproj';
+
 }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import {AppComponent} from "../app.component";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
+
+
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  constructor() {
+
+  }
 
   ngOnInit(): void {
   }
