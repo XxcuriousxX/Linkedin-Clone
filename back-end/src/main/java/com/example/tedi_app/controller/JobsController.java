964ac0bc8489e7d0abb55e56f8f1074b19009bae
kkmanos <koukoularisman@gmail.com>
2021-07-29 22:08:57 +0300

    added grid in my network

---
+++ back-end/src/main/java/com/example/tedi_app/controller/JobsController.java
@@ -0,0 +1,9 @@
+package com.example.tedi_app.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/messages")
+public class JobsController {
+}
